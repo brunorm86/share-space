fb4a8e785a571474832e75761629fdadaf8e8a42
Bruno Ricardo Machado <bruno.rm@grad.ufsc.br>
2025-10-02 20:06:56 -0300

    App comentarios created.

--- setup/settings.py
+++ setup/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'galeria.apps.GaleriaConfig',
     'usuarios.apps.UsuariosConfig',
-    'storages'
+    'comentarios.apps.ComentariosConfig'
 ]
 
 MIDDLEWARE = [
